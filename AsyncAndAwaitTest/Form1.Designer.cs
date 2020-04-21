c53d1d6d27e3ef56cd7e7a439fded6fb860c79a2
陈昌添 <fudacct@163.com>
2020-04-21 21:57:02 +0800

    add Async Await example

---
+++ AsyncAndAwaitTest/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace AsyncAndAwaitTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Async = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btn_Task = new System.Windows.Forms.Button();
+            this.btn_SimpleAsync = new System.Windows.Forms.Button();
+            this.btn_Clear = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_Async
+            // 
+            this.btn_Async.Location = new System.Drawing.Point(12, 12);
+            this.btn_Async.Name = "btn_Async";
+            this.btn_Async.Size = new System.Drawing.Size(106, 23);
+            this.btn_Async.TabIndex = 0;
+            this.btn_Async.Text = "AsyncTest";
+            this.btn_Async.UseVisualStyleBackColor = true;
+            this.btn_Async.Click += new System.EventHandler(this.btn_Async_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(170, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(618, 426);
+            this.textBox1.TabIndex = 1;
+            // 
+            // btn_Task
+            // 
+            this.btn_Task.Location = new System.Drawing.Point(13, 42);
+            this.btn_Task.Name = "btn_Task";
+            this.btn_Task.Size = new System.Drawing.Size(105, 23);
+            this.btn_Task.TabIndex = 2;
+            this.btn_Task.Text = "TaskTest";
+            this.btn_Task.UseVisualStyleBackColor = true;
+            this.btn_Task.Click += new System.EventHandler(this.btn_Task_Click);
+            // 
+            // btn_SimpleAsync
+            // 
+            this.btn_SimpleAsync.Location = new System.Drawing.Point(12, 72);
+            this.btn_SimpleAsync.Name = "btn_SimpleAsync";
+            this.btn_SimpleAsync.Size = new System.Drawing.Size(106, 23);
+            this.btn_SimpleAsync.TabIndex = 3;
+            this.btn_SimpleAsync.Text = "SimpleAsync";
+            this.btn_SimpleAsync.UseVisualStyleBackColor = true;
+            this.btn_SimpleAsync.Click += new System.EventHandler(this.btn_SimpleAsync_Click);
+            // 
+            // btn_Clear
+            // 
+            this.btn_Clear.Location = new System.Drawing.Point(13, 102);
+            this.btn_Clear.Name = "btn_Clear";
+            this.btn_Clear.Size = new System.Drawing.Size(105, 23);
+            this.btn_Clear.TabIndex = 4;
+            this.btn_Clear.Text = "Clear";
+            this.btn_Clear.UseVisualStyleBackColor = true;
+            this.btn_Clear.Click += new System.EventHandler(this.btn_Clear_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 132);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "returnObject";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 415);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(13, 386);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn_Clear);
+            this.Controls.Add(this.btn_SimpleAsync);
+            this.Controls.Add(this.btn_Task);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btn_Async);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Async;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btn_Task;
+        private System.Windows.Forms.Button btn_SimpleAsync;
+        private System.Windows.Forms.Button btn_Clear;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ AsyncAndAwaitTest/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsyncAndAwaitTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btn_Async_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Console.WriteLine("test");
+                //textBox1.Text = "111 balabala. my thread id is:" + Thread.CurrentThread.ManagedThreadId + "\r\n";
+                ShowMessage("111 balabala. my thread id is:" + Thread.CurrentThread.ManagedThreadId);
+                var resultTask = MethodAsync();
+                textBox1.Text += await resultTask;
+                //textBox1.Text += "222 balabala. my thread id is:" + Thread.CurrentThread.ManagedThreadId + "\r\n";
+                ShowMessage("222 balabala. my thread id is:" + Thread.CurrentThread.ManagedThreadId);
+            }
+            catch (Exception ex)
+            {
+                ShowMessage(ex.Message);
+            }
+            
+        }
+
+        private async Task<string> MethodAsync()
+        {
+            var resultFromTimeConsumingMethod = TimeConsumingMethod();
+            string result = await resultFromTimeConsumingMethod + " + MethodAsync. my thread id is:" + Thread.CurrentThread.ManagedThreadId + "\r\n";
+            //textBox1.Text += result;
+            ShowMessage(result);
+            return result;
+        }
+
+        private Task<string> TimeConsumingMethod()
+        {
+            var task = Task.Run(() =>
+            {
+                //textBox1.Text += "Helo I am TimeConsumingMethod. My Thread ID is :" + Thread.CurrentThread.ManagedThreadId + "/r/n";
+                ShowMessage("Helo I am TimeConsumingMethod. My Thread ID is :" + Thread.CurrentThread.ManagedThreadId);
+                Thread.Sleep(5000);
+                //textBox1.Text += "Helo I am TimeConsumingMethod after Sleep(5000). My Thread ID is :" + Thread.CurrentThread.ManagedThreadId + "/r/n";
+                ShowMessage("Helo I am TimeConsumingMethod after Sleep(5000). My Thread ID is :" + Thread.CurrentThread.ManagedThreadId);
+                Thread.Sleep(5000);
+                return "Hello I am TimeConsumingMethod";
+            });
+            return task;
+        }
+
+        private async Task<string> MethodAsyncReturnObject()
+        {
+            var resultFromTimeConsumingMethod = TimeConsumingMethodReturnObject();
+            ReturnObject returnObject = resultFromTimeConsumingMethod.Result;
+            if (returnObject.Flag)
+            {
+
+            }
+            string result = await resultFromTimeConsumingMethod + " + MethodAsync. my thread id is:" + Thread.CurrentThread.ManagedThreadId + "\r\n";
+            //textBox1.Text += result;
+            ShowMessage(result);
+            return result;
+        }
+
+        private async Task<ReturnObject> TimeConsumingMethodReturnObject()
+        {
+            var task = Task.Run(() =>
+            {
+                ReturnObject returnObject = new ReturnObject();
+                returnObject.ReturnMessage1 = "Helo I am TimeConsumingMethod. My Thread ID is :" + Thread.CurrentThread.ManagedThreadId;
+                Thread.Sleep(5000);
+                returnObject.ReturnMessage2 = "Helo I am TimeConsumingMethod after Sleep(5000). My Thread ID is :" + Thread.CurrentThread.ManagedThreadId;
+                returnObject.ReturnMessage3 = "Hello I am TimeConsumingMethod";
+                returnObject.Flag = true;
+                return returnObject;
+            });
+            return await task;
+        }
+
+        private void ShowMessage(string message)
+        {
+            if (!textBox1.InvokeRequired)
+                //textBox1.Text += message + "\r\n";
+                textBox1.AppendText(message + "\r\n");
+            else
+                textBox1.Invoke(new Action<string>(ShowMessage), new object[] { message });
+        }
+
+
+        private void btn_Clear_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+        private async void btn_SimpleAsync_Click(object sender, EventArgs e)
+        {
+            //var t = Task.Run(() =>
+            //{
+
+            //});
+            var resultTask = MethodAsync();
+            textBox1.Text += await resultTask;
+        }
+
+        private void btn_Task_Click(object sender, EventArgs e)
+        {
+            var resultTask = Task.Run(() =>
+            {
+                ShowMessage("Helo I am TimeConsumingMethod. My Thread ID is :" + Thread.CurrentThread.ManagedThreadId);
+                Thread.Sleep(5000);
+                ShowMessage("Helo I am TimeConsumingMethod after Sleep(5000). My Thread ID is :" + Thread.CurrentThread.ManagedThreadId);
+                return "Hello I am TimeConsumingMethod";
+            });
+            resultTask.ContinueWith(OnDoSomthingIsComplete);
+        }
+
+        private void OnDoSomthingIsComplete(Task<string> t)
+        {
+            Action action = () => {
+                textBox1.Text = t.Result+"\r\n";
+            };
+            textBox1.Invoke(action);
+            ShowMessage("Continue Thread ID :" + Thread.CurrentThread.ManagedThreadId);
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            var resultTask = TimeConsumingMethodReturnObject();
+            var returnResult = await resultTask;
+            if (returnResult.Flag)
+            {
+                //textBox1.Text += returnResult.ReturnMessage1 + "\r\n";
+                //textBox1.Text += returnResult.ReturnMessage2 + "\r\n";
+                //textBox1.Text += returnResult.ReturnMessage3 + "\r\n";
+                textBox1.AppendText(returnResult.ReturnMessage1 + "\r\n");
+                textBox1.AppendText(returnResult.ReturnMessage2 + "\r\n");
+                textBox1.AppendText(returnResult.ReturnMessage3 + "\r\n");
+            }
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                //if (i == 3)
+                //    Thread.Sleep(100);
+                //else
+                //Thread.Sleep(10);
+                textBox1.AppendText(( MethodAsync2(i))+ "\r\n");
+                Thread.Sleep(50);
+                textBox1.AppendText(string.Format("main thread output:{0};thread id is:{1};\r\n", i, Thread.CurrentThread.ManagedThreadId));
+            }
+        }
+
+        private async Task<ReturnObject> MethodAsync2(int inputValue)
+        {
+            ReturnObject returnObject = new ReturnObject();
+            var task = Task.Run(() =>
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    if (inputValue % 2 == 0)
+                        Thread.Sleep(2000);
+                    ShowMessage(string.Format("async function output: input values is :{0};thread id is:{1};runing count is:{2}", inputValue, Thread.CurrentThread.ManagedThreadId, i));
+                    //Action action = () => {
+                    //    textBox1.AppendText(string.Format("input values is :{0};thread id is:{1};runing count is:{2}\r\n", inputValue, Thread.CurrentThread.ManagedThreadId,i));
+                    //};
+                    //textBox1.Invoke(action);
+                }
+                return "you input is:"+ inputValue;
+            });
+            returnObject.ReturnMessage1 = await task;
+            returnObject.Flag = true;
+            ShowMessage(string.Format("111:{0}", inputValue));
+            return returnObject;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.AppendText("hello world!\r\n");
+        }
+    }
+
+    public class ReturnObject
+    {
+        public string ReturnMessage1 { get; set; }
+        public string ReturnMessage2 { get; set; }
+        public string ReturnMessage3 { get; set; }
+        public bool Flag { get; set; }
+    }
+
+
+
+}
